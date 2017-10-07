b896e3df84ffcc68b42e73e23fdc4423f88de15d
Vinod Keshav <vinnzy@gmail.com>
2017-10-07 20:22:26 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+    
+        'url' : 'mongodb://127.0.0.1:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    
+    };
+    
+    
